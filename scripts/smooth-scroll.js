959e427b2c629af5e8c68ebb05c50df611b6eec8
ArthurBilyk02 <arthurbilyk82@gmail.com>
2025-01-30 19:28:55 +0000

    Fixed Buttons

--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,16 +1,23 @@
-$(document).ready(function() {
-    console.log("smooth-scroll.js loaded!");
-  
-    // Smooth Scrolling for Navigation Links
-    $("nav ul li a, .ui.menu a").on("click", function(event) {
-      event.preventDefault();
-      let targetId = $(this).attr("href").substring(1);
-      let targetSection = $("#" + targetId);
-  
-      if (targetSection.length) {
-        $("html, body").animate({
-          scrollTop: targetSection.offset().top
-        }, 600);
-      }
+            $(document).ready(function() {
+            console.log("smooth-scroll.js loaded!");
+
+            // Smooth Scrolling for Navigation Links
+            $("nav ul li a, .ui.menu a").on("click", function(event) {
+            event.preventDefault();
+
+            let targetId = $(this).attr("href").substring(1);
+
+            if (targetId === "home") {
+            // Scroll all the way to the top when clicking "Home"
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            } else {
+            let targetSection = $("#" + targetId);
+
+            if (targetSection.length) {
+            $("html, body").animate({
+                scrollTop: targetSection.offset().top
+            }, 600);
+            }
+        }
     });
-  });
+});
